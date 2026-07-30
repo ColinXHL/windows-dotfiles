c538160e4eecc0e9d530c655e049ac78e67e2be0
Colin Xu <127581131+ColinXHL@users.noreply.github.com>
2026-07-30 13:27:55 +0800

    Increase WezTerm background transparency

--- wezterm/modules/appearance.lua
+++ wezterm/modules/appearance.lua
@@ -25,7 +25,7 @@ function M.apply_to_config(config)
 	-- }
 
 	config.color_scheme = "Tokyo Night Moon"
-	config.window_background_opacity = 0.95
+	config.window_background_opacity = 0.82
 	config.win32_system_backdrop = "Acrylic"
 	-- Keep the rainy Tokyo artwork moving subtly with terminal scrollback.
 	config.background = {
